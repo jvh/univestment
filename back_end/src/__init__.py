9a7b209784d4ff91292226d20c420f6b008a684f
jvh <jonathan@jvharper.com>
2019-05-10 18:00:29 +0100

    Added postgres stuff

--- back_end/src/__init__.py
+++ back_end/src/__init__.py
@@ -9,5 +9,10 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 # Zoopla's API key
 ZOOPLAAPIKEY = "x3augy4g7kxxm3m3tcanc5xd"
+POSTGRES_PASSWORD = 'r$B4wnH*HSB#v2W'
+POSTGRES_USERNAME = 'comp6214_team'
+POSTGRES_PORT = "5432"
+POSTGRES_IP = "127.0.0.1"
+POSTGRES_DATABASE = 'housing_data'
 # Set to be True when development is occurring (False for preduction)
 DEVELOPMENT = True
--- back_end/src/import_data_to_db.py
+++ back_end/src/import_data_to_db.py
@@ -1,17 +1,38 @@
 import psycopg2
+from back_end.src import POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_IP, POSTGRES_PORT, POSTGRES_DATABASE
+
+def create_tables():
+    house_price_data = \
+        'CREATE TABLE IF NOT EXISTS house_price_data (' \
+        '   id INTEGER PRIMARY KEY,' \
+        '   price FLOAT NOT NULL,' \
+        '   date_of_transfer DATE,' \
+        '   postcode TEXT NOT NULL,' \
+        '   property_type TEXT,' \
+        '   is_newly_built BOOL,' \
+        '   duration TEXT,' \
+        '   primary_addressable_object_name TEXT,' \
+        '   secondary_addressable_object_name TEXT,' \
+        '   street TEXT,' \
+        '   locality TEXT,' \
+        '   town_city TEXT,' \
+        '   district TEXT,' \
+        '   county TEXT,' \
+        '   price_paid_transaction_type TEXT' \
+        ');'
+    yield house_price_data
 
 try:
-    connection = psycopg2.connect(user = "sysadmin",
-                                  password = "pynative@#29",
-                                  host = "127.0.0.1",
-                                  port = "5432",
-                                  database = "postgres_db")
+    connection = psycopg2.connect(user=POSTGRES_USERNAME,
+                                  password=POSTGRES_PASSWORD,
+                                  host=POSTGRES_IP,
+                                  port=POSTGRES_PORT,
+                                  database=POSTGRES_DATABASE)
     cursor = connection.cursor()
-    # Print PostgreSQL Connection properties
-    print ( connection.get_dsn_parameters(),"\n")
-    # Print PostgreSQL version
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("You are connected to - ", record,"\n")
+
+    for table_command in create_tables():
+        cursor.execute(table_command)
+
 except (Exception, psycopg2.Error) as error :
-    print ("Error while connecting to PostgreSQL", error)
+    print ("Error connecting to postgres: ", error)
+
