a72d6da04e3790f693288e7e5d31b573e1f55dd6
jvh <jonathan@jvharper.com>
2019-05-10 10:44:34 +0100

    Changed var name

--- back_end/src/zoopla_ingest.py
+++ back_end/src/zoopla_ingest.py
@@ -1,4 +1,4 @@
-from zoopla import Zoopla, exceptions as ZooplaExceptions
+from zoopla import Zoopla, exceptions as zoopla_exceptions
 
 from back_end.src import constants
 
@@ -19,14 +19,14 @@ class ZooplaIngest:
         try:
             listings = self.zoopla.property_listings(params)
             return listings
-        except ZooplaExceptions.RequestFormatException:
-            raise ZooplaExceptions.RequestFormatException("Invalid parameters for property listing request. See"
+        except zoopla_exceptions.RequestFormatException:
+            raise zoopla_exceptions.RequestFormatException("Invalid parameters for property listing request. See"
                                                           "Zoopla API docs for valid parameters.")
-        except ZooplaExceptions.ResponseFormatException:
-            raise ZooplaExceptions.ResponseFormatException("Property listing request returned unexpected parameters."
+        except zoopla_exceptions.ResponseFormatException:
+            raise zoopla_exceptions.ResponseFormatException("Property listing request returned unexpected parameters."
                                                            "This may be due to changes in the Zoopla API.")
-        except ZooplaExceptions.ZooplaAPIException as e:
-            raise ZooplaExceptions.ZooplaAPIException(str(e))
+        except zoopla_exceptions.ZooplaAPIException as e:
+            raise zoopla_exceptions.ZooplaAPIException(str(e))
 
 
 if __name__ == "__main__":
