be6f1d8c2a4878becfd096ee2ae2a24635433d05
SHassan12 <shahaan.hassan@gmail.com>
2019-05-12 11:44:17 +0100

    query for postcodes in progress

--- back_end/src/response_processing.py
+++ back_end/src/response_processing.py
@@ -1,7 +1,16 @@
+from back_end.src.import_data_to_db import DatabaseHandler
 
-class AdzunaResponseProcessor():
+
+class AdzunaResponseProcessor:
 
     def __init__(self):
-        pass
+        self.db = DatabaseHandler
 
-    def
+    def query_by_postcode(self, response_data):
+        returned_house_prices = []
+        for response in response_data:
+            postcode = response.get("postcode")
+            if postcode is not None:
+                query_results = self.db.query_database("SELECT * FROM house_price_data WHERE "
+                                                       "postcode = '{}'".format(postcode))
+                returned_house_prices = returned_house_prices + query_results
