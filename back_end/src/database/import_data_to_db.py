85ad96cb5fc715132f955d2d9adc40fd7d6e44a7
jvh <jonathan@jvharper.com>
2019-05-13 11:31:03 +0100

    Fixed geolocation

--- back_end/src/database/import_data_to_db.py
+++ back_end/src/database/import_data_to_db.py
@@ -183,8 +183,8 @@ class DatabaseHandler:
         data['id'] = [uuid4() for _ in range(len(data.index))]
         data.to_sql('admissions_data', engine, if_exists="fail", index=False)
 
-    @staticmethod
-    def fill_uni_addresses(engine, import_files):
+    # @staticmethod
+    def fill_uni_addresses(self, engine, import_files):
         """
         Store university address data in uni_addresses_data table
 
@@ -198,7 +198,7 @@ class DatabaseHandler:
         latitude = []
         data = data[pd.notnull(data['postcode'])]
         for row in data['postcode']:
-            long, lat = geo_locations.get_coords_from_postcode(row)
+            long, lat = self.geo_locations.get_coords_from_postcode(row)
             longitude.append(long)
             latitude.append(lat)
         data['longitude'] = longitude
