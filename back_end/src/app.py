6a6a7460814408a637a5e27447c1cf727ec7cf66
jvh <jonathan@jvharper.com>
2019-05-14 03:52:36 +0100

    Cleaned up code a lot

--- back_end/src/app.py
+++ back_end/src/app.py
@@ -92,35 +92,13 @@ def format_params(params):
     return params
 
 
-def get_property_args(p, large_images):
+def query_already_processed(query_id):
     """
-    For a given advertisement, get the property parameters for table insertion
-    :param p: The property
-    :param large_images: The large image results
-    :return: The parameters for insertion into table
-    """
-    uni = None
-    lrg = False
-    if 'university' in p:
-        uni = p['university']
-    if p in large_images:
-        lrg = True
-    params = (p['id'], p['beds'], p['description'], p['image_url'], p['is_furnished'], p['latitude'], p['longitude'],
-              p['postcode'], p['property_type'], p['redirect_url'], p['sale_price'], p['title'], uni, lrg)
-    return params
+    If a query has already been processed, get its results
 
-
-@app.route('/search')
-def query_property_listing():
+    :param query_id: ID of the query
+    :return: If it has been processed, return list of results
     """
-    Query the Adzuna API for property listings using the received parameters
-
-    :return: Property listing
-    """
-    results = []
-    params = request.args.to_dict()
-    query_id = uuid.uuid3(uuid.NAMESPACE_DNS, str(sorted(params)))
-    query_id = psql_extras.UUID_adapter(query_id)
     query = "SELECT properties FROM seen_queries WHERE id={}".format(query_id)
     if_processed = DatabaseHandler.query_database(query)
 
@@ -158,6 +136,117 @@ def query_property_listing():
             results_from_db_lrg.append(record)
 
         return jsonify(results_from_db_lrg)
+
+
+def get_properties_near_unis(params):
+    """
+    Returns all those properties within the vicinity of a university
+
+    :param params: The parameters passed
+    :return: The list of properties near the universities
+    """
+    nearby_unis = geo_locations.get_universities_near_location(params['where'], params['radius_from'])
+
+    if not nearby_unis:
+        return jsonify({"error": "No universities within area specified."})
+
+    # The set of properties surrounding those universities
+    properties = []
+
+    # Searching for houses from each university
+    for uni in nearby_unis:
+        post = uni[3]
+        uni_params = deepcopy(params)
+        uni_params['where'] = post
+        uni_params['distance'] = params['km_away_from_uni']
+
+        # Formatting parameters for use by adzuna
+        uni_params = format_params(uni_params)
+        property_listing = adzuna.get_property_listing(uni_params)
+        results = property_listing.get("results")
+
+        for r in results:
+            # Assigning that property to a particular university
+            r['university'] = uni[0]
+            if r not in properties:
+                properties.append(r)
+
+    return properties
+
+
+def get_property_args(p, large_images):
+    """
+    For a given advertisement, get the property parameters for table insertion
+    :param p: The property
+    :param large_images: The large image results
+    :return: The parameters for insertion into table
+    """
+    uni = None
+    lrg = False
+    if 'university' in p:
+        uni = p['university']
+    if p in large_images:
+        lrg = True
+    params = (p['id'], p['beds'], p['description'], p['image_url'], p['is_furnished'], p['latitude'], p['longitude'],
+              p['postcode'], p['property_type'], p['redirect_url'], p['sale_price'], p['title'], uni, lrg)
+    return params
+
+
+def populate_seen_tables(results, large_images, query_id, params):
+    """
+    When a new query is ran, populate the seen tables (seen_queries and seen_adverts) with their corresponding data.
+
+    :param results: The resulting advertisements given by a query
+    :param large_images: The list of results which have large images
+    :param query_id: The UUID of the query
+    :param params: Params given by the user for the query
+    """
+    # The list of property IDs in the results (of the advertisements)
+    property_id_list = []
+
+    # Adding advertisements to seen_adverts table
+    for r in results:
+        property_id_list.append(r['id'])
+        args = (r['id'],)
+
+        # Identify if the property advertisement has already been seen by the table
+        seen_property_listing = "SELECT id FROM seen_adverts WHERE id=%s;"
+        seen_before = DatabaseHandler.query_database(seen_property_listing, args)
+
+        # If it has been seen, update date
+        if seen_before:
+            search_property_query = "UPDATE seen_adverts SET date_of_insertion=DEFAULT WHERE id=%s;"
+            DatabaseHandler.insert_to_db(search_property_query, args)
+        else:
+            # Add it to the table
+            add_property_query = "INSERT INTO seen_adverts VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, " \
+                                 "%s, %s, %s, DEFAULT, %s);"
+            args = get_property_args(r, large_images)
+            DatabaseHandler.insert_to_db(add_property_query, args)
+
+    # Add it to the table seen_queries
+    add_query = "INSERT INTO seen_queries VALUES (%s, %s, %s, DEFAULT);"
+    args = (query_id, str(params), ' '.join(str(e) for e in property_id_list))
+    DatabaseHandler.insert_to_db(add_query, args)
+
+
+@app.route('/search')
+def query_property_listing():
+    """
+    Query the Adzuna API for property listings using the received parameters
+
+    :return: Property listing
+    """
+    params = request.args.to_dict()
+    query_id = uuid.uuid3(uuid.NAMESPACE_DNS, str(sorted(params)))
+    query_id = psql_extras.UUID_adapter(query_id)
+
+    # If query has already been processed, get results
+    already_processed = query_already_processed(query_id)
+    if already_processed:
+        return already_processed
+
+    # Query has not been processed before and therefore must be processed as new
     try:
         # If the user has selected they're searching for student rental opportunities
         if "search_student_lets" in params and params["search_student_lets"] == 'true':
@@ -170,33 +259,7 @@ def query_property_listing():
                 raise Exception("Please enter the distance from any given university (in km) that you would like to "
                                 "search houses for.")
 
-            nearby_unis = geo_locations.get_universities_near_location(params['where'], params['radius_from'])
-
-            if not nearby_unis:
-                return jsonify({"error": "No universities within area specified."})
-
-            # The set of properties surrounding those universities
-            properties = []
-
-            # Searching for houses from each university
-            for uni in nearby_unis:
-                post = uni[3]
-                uni_params = deepcopy(params)
-                uni_params['where'] = post
-                uni_params['distance'] = params['km_away_from_uni']
-
-                # Formatting parameters for use by adzuna
-                uni_params = format_params(uni_params)
-                property_listing = adzuna.get_property_listing(uni_params)
-                results = property_listing.get("results")
-
-                for r in results:
-                    # Assigning that property to a particular university
-                    r['university'] = uni[0]
-                    if r not in properties:
-                        properties.append(r)
-
-                results = properties
+            results = get_properties_near_unis(params)
         else:
             # If the user hasn't specified they are exclusively looking for student homes
             params = format_params(params)
@@ -209,32 +272,8 @@ def query_property_listing():
             # Obtain all of those results which have large images available
             large_images = large_images_only(results)
 
-            property_id_list = []
-
-            # Adding advertisements to seen_adverts table
-            for r in results:
-                property_id_list.append(r['id'])
-                args = (r['id'],)
-
-                # Identify if the property advertisement has already been seen by the table
-                seen_property_listing = "SELECT id FROM seen_adverts WHERE id=%s;"
-                seen_before = DatabaseHandler.query_database(seen_property_listing, args)
-
-                # If it has been seen, update date
-                if seen_before:
-                    search_property_query = "UPDATE seen_adverts SET date_of_insertion=DEFAULT WHERE id=%s;"
-                    DatabaseHandler.insert_to_db(search_property_query, args)
-                else:
-                    # Add it to the table
-                    add_property_query = "INSERT INTO seen_adverts VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, " \
-                                         "%s, %s, %s, DEFAULT, %s);"
-                    args = get_property_args(r, large_images)
-                    DatabaseHandler.insert_to_db(add_property_query, args)
-
-            # Add it to the table seen_queries
-            add_query = "INSERT INTO seen_queries VALUES (%s, %s, %s, DEFAULT);"
-            args = (query_id, str(params), ' '.join(str(e) for e in property_id_list))
-            DatabaseHandler.insert_to_db(add_query, args)
+            # Populates seen_queries and seen_adverts tables with results of query
+            populate_seen_tables(results, large_images, query_id, params)
 
             results = large_images
 
