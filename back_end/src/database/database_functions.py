e47b0dfd634d51b3a5df2f48ea6775201633b22a
jvh <jonathan@jvharper.com>
2019-05-15 16:12:41 +0100

    Moved database function stuff from response_processing

--- back_end/src/database/database_functions.py
+++ back_end/src/database/database_functions.py
@@ -64,3 +64,74 @@ def query_database(query, params=""):
         return result
     except (Exception, psycopg2.Error) as error :
         print("Error connecting to postgres: ", error)
+
+
+def query_by_postcode(postcode):
+    """
+    Query database for properties by postcode
+
+    :param postcode: String - a postcode
+    :return:
+    """
+    query = "SELECT * FROM house_price_data WHERE postcode = '{}' ORDER BY date_of_transfer;".format(postcode)
+    results = query_database(query)
+    return results
+
+
+def query_for_price_data(outcode):
+    """
+    Query database for predicted admissions by university
+
+    :param outcode: String - an outcode
+    :return: query result
+    """
+    query = "SELECT historical_data, prediction_data FROM predictions_data WHERE outcode = '{}';".format(outcode)
+    results = query_database(query)
+    return results
+
+
+def query_admission(university):
+    """
+    Query database for admissions by university
+
+    :param university: String - name of university
+    :return: query result
+    """
+    query = "SELECT * FROM admissions_data WHERE university = '{}'".format(university)
+    results = query_database(query)
+    return results
+
+
+def query_predicted_admission_data(university):
+    """
+    Query database for predicted admissions by university
+
+    :param university: String - name of university
+    :return: query result
+    """
+    query = "SELECT historic_admissions, predicted_admissions FROM predicted_admissions_table WHERE " \
+            "university = '{}'".format(university)
+    results = query_database(query)
+
+    return results
+
+
+def query_by_outcode(outcodes):
+    """
+    Query database for properties by outcode and insert market value predictions into database
+
+    :param outcodes: A list of outcodes
+    """
+    for outcode in outcodes:
+        query = "SELECT outcode FROM predictions_data WHERE outcode = '{}'".format(outcode)
+        result = query_database(query)
+
+        if not result:
+            query_results_area = query_database("SELECT * FROM house_price_data WHERE substr(postcode,1,{})"
+                                                        " = '{}' ORDER BY date_of_transfer;"
+                                                        .format(len(outcode), outcode))
+
+            # returned_house_prices_area = returned_house_prices_area + query_results_area
+            if query_results_area:
+                start_date, historic_data, predicted_data = generate_prediction(query_results_area)
+                insert_predictions(outcode, start_date, historic_data, predicted_data)
