12d3e746dc71445c17da41dab1abbd8dfa11276c
SHassan12 <shahaan.hassan@gmail.com>
2019-05-15 19:32:39 +0100

    inital search by distance to uni function

--- back_end/src/uni_nearby_ads.py
+++ back_end/src/uni_nearby_ads.py
@@ -1,5 +1,27 @@
-
+from back_end.src.database.generic_db_functions import query_database, insert_to_db
+import requests
 
 
 def get_ads_near_uni(university):
+    url = "localhost:5005/search"
+
     query = "SELECT postcode FROM uni-addresses-data WHERE establishmentname = '{}';".format(university)
+    uni_postcode = query_database(query)
+
+    if uni_postcode:
+        distance = 2
+
+        params = dict()
+        params["where"] = uni_postcode
+        params["distance"] = distance
+        params["testing"] = "uni_nearby_ads"
+
+        response = requests.get(url, params=params)
+        if response:
+            ids = []
+            for r in response:
+                ids.append(r[id])
+
+            params = (response[university, distance, ids])
+            query = "INSERT INTO distance_from_uni_data VALUES (%s, %s, %s)"
+            insert_to_db(query, params)
